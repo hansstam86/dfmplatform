422bf3d9abd7bd36a1086b2aaeaf34e5a0a3d393
hansstam86 <134194676+hansstam86@users.noreply.github.com>
2026-03-23 16:40:55 +0100

    Delete route.ts

--- route.ts
+++
@@ -1,8 +0,0 @@
-import { NextRequest, NextResponse } from 'next/server'
-import { createServerSupabaseClient } from '@/lib/supabase-server'
-
-export async function POST(req: NextRequest) {
-  const supabase = createServerSupabaseClient()
-  await supabase.auth.signOut()
-  return NextResponse.redirect(new URL('/auth/login', req.url))
-}
